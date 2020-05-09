24b4621fe6c04473b69a1beb7e36cbd30fac3cfb
Benjamin Ozore <benjaminozore@yahoo.com>
2020-05-09 14:02:26 -0500

    a working search bar that outputs value to console and a working api fetch

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const apiKey = `zqAH1xZnEZviNZHUaz6`;
 const form = document.querySelector('form');
 
-fetch(`https://api.winnipegtransit.com/v3/streets.json?api-key=${apiKey}&name=henlow`)
-.then(resp => resp.json())
-.then(json => console.log(json));
+form.onsubmit = event => {
+  const input = event.target.querySelector('input');
+  searchBus(input.value);
+  console.log(input.value);
+  event.preventDefault();
+}
+
+function searchBus(query) {
+  fetch(`https://api.winnipegtransit.com/v3/streets.json?api-key=${apiKey}&name=${query}`)
+    .then(resp => {
+      if (resp.ok) {
+        return resp.json();
+      } else {
+        throw new Error("Error problem.");
+      }
+    })
+}
+
