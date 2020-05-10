0ee160536dce3a79615ca7c0606a2ee40f33dbb8
Benjamin Ozore <benjaminozore@yahoo.com>
2020-05-10 12:59:41 -0500

    prints the array data to the console after fetch

--- app.js
+++ app.js
@@ -1,31 +1,19 @@
 const apiKey = `zqAH1xZnEZviNZHUaz6`;
-const form = document.querySelector('form');
-const streets = document.querySelector('.streets');
+const form = document.querySelector("form");
+const streetsEle = document.querySelector(".streets");
 
-form.onsubmit = event => {
-  const input = event.target.querySelector('input');
+form.onsubmit = (event) => {
+  const input = event.target.querySelector("input");
   searchBus(input.value);
-  console.log(input.value);
   event.preventDefault();
-
-  streets.insertAdjacentHTML('afterbegin', `
-    <a href="#" data-street-key="4499">${input.value}</a> 
-  `)
-}
+};
 
 function searchBus(query) {
-  fetch(`https://api.winnipegtransit.com/v3/streets.json?api-key=${apiKey}&name=${query}?usage=long`)
-    .then(resp => {
-      if (resp.ok) {
-        return resp.json();
-      } else {
-        throw new Error("Error problem.");
-      }
-    })
+  fetch(`https://api.winnipegtransit.com/v3/streets.json?api-key=${apiKey}&name=${query}`)
+    .then((resp) => resp.json())
+    .then((data) => {
+      console.log(data.streets);
+    });
 }
 
-
-
-
-//  <a href="#" data-street-key="4499">Kenaston Common Drive</a> 
 
