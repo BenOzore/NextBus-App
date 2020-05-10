7e93bcef711abbe52d1e6c6086e59d2ac96c50aa
Benjamin Ozore <benjaminozore@yahoo.com>
2020-05-10 13:47:11 -0500

    made slight change to variable names

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const apiKey = `zqAH1xZnEZviNZHUaz6`;
 const form = document.querySelector("form");
 const streetsEle = document.querySelector(".streets");
+const titleBar = document.querySelector('main').firstElementChild;
 
 form.onsubmit = (event) => {
   const input = event.target.querySelector("input");
@@ -9,11 +10,20 @@ form.onsubmit = (event) => {
 };
 
 function searchBus(query) {
-  fetch(`https://api.winnipegtransit.com/v3/streets.json?api-key=${apiKey}&name=${query}`)
+  fetch(
+    `https://api.winnipegtransit.com/v3/streets.json?api-key=${apiKey}&name=${query}`
+  )
     .then((resp) => resp.json())
-    .then((data) => {
-      console.log(data.streets);
+    .then((json) => {
+      insertIntoStreetEle(json.streets);
     });
 }
 
-
+function insertIntoStreetEle(data) {
+  streetsEle.innerHTML = "";
+  data.forEach(function (street) {
+    streetsEle.insertAdjacentHTML(
+      "afterbegin", `<a href="#" data-street-key=${street.key}>${street.name}</a> `
+    );
+  });
+}
