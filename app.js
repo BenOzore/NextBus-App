0367653295ca1fbd8bf94e9f1b195676a67fb531
Benjamin Ozore <benjaminozore@yahoo.com>
2020-05-11 12:52:56 -0500

    retrieve stops data using the winnipeg transit api through clicking the search result performed by the user

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 const apiKey = `zqAH1xZnEZviNZHUaz6`;
 const form = document.querySelector("form");
 const streetsEle = document.querySelector(".streets");
-const titleBar = document.querySelector('main').firstElementChild;
-
-form.onsubmit = (event) => {
-  const input = event.target.querySelector("input");
-  searchBus(input.value);
-  event.preventDefault();
-};
+const titleBar = document.querySelector("main").firstElementChild;
+let streetObject;
+let stopArray ;
 
 function searchBus(query) {
   fetch(
@@ -15,7 +11,8 @@ function searchBus(query) {
   )
     .then((resp) => resp.json())
     .then((json) => {
-      insertIntoStreetEle(json.streets);
+      streetObject = json.streets;
+      insertIntoStreetEle(streetObject);
     });
 }
 
@@ -23,7 +20,29 @@ function insertIntoStreetEle(data) {
   streetsEle.innerHTML = "";
   data.forEach(function (street) {
     streetsEle.insertAdjacentHTML(
-      "afterbegin", `<a href="#" data-street-key=${street.key}>${street.name}</a> `
+      "afterbegin",
+      `<a href="#" data-street-key=${street.key}>${street.name}</a> `
     );
   });
 }
+
+streetsEle.onclick = (event) => {
+  if (event.target.nodeName === "A") {
+    stopArray = event.target.dataset.streetKey;
+    fetch(
+      `https://api.winnipegtransit.com/v3/stops.json?street=${stopArray}&api-key=${apiKey}`
+    )
+      .then((resp) => resp.json())
+      .then((data) => {
+        for(const key in data.stops) {
+          console.log(data.stops[key]);
+        }
+      });
+  }
+};
+
+form.onsubmit = (event) => {
+  const input = event.target.querySelector("input");
+  searchBus(input.value);
+  event.preventDefault();
+};
