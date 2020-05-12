ddf401053bc7b0280b85ac52f63b3e7c5225fe1f
Benjamin Ozore <benjaminozore@yahoo.com>
2020-05-11 19:06:31 -0500

    time stamp for next bus has been added to DOM and update to title bar

--- app.js
+++ app.js
@@ -2,51 +2,62 @@ const apiKey = `zqAH1xZnEZviNZHUaz6`;
 const form = document.querySelector("form");
 const streetsEle = document.querySelector(".streets");
 const titleBar = document.querySelector("main").firstElementChild;
-let streetObject;
-let stopArray ;
 const tbody = document.querySelector('tbody');
+let streetObject;
+let streetKey;
+let stopsObject;
+let stopScheduleObject;
+let x = [];
 
 function searchBus(query) {
-  fetch(
-    `https://api.winnipegtransit.com/v3/streets.json?api-key=${apiKey}&name=${query}`
-  )
+  fetch(`https://api.winnipegtransit.com/v3/streets.json?api-key=${apiKey}&name=${query}`)
     .then((resp) => resp.json())
     .then((json) => {
       streetObject = json.streets;
-      insertIntoStreetEle(streetObject);
+      insertIntoStreetList(streetObject);
     });
-}
-
-function insertIntoStreetEle(data) {
+  }
+  
+function insertIntoStreetList(data) {
   streetsEle.innerHTML = "";
-  data.forEach(function (street) {
+  data.forEach(function (streetData) {
     streetsEle.insertAdjacentHTML(
       "afterbegin",
-      `<a href="#" data-street-key=${street.key}>${street.name}</a> `
+      `<a href="#" data-street-key=${streetData.key}>${streetData.name}</a> `
     );
   });
 }
 
 streetsEle.onclick = (event) => {
   if (event.target.nodeName === "A") {
-    stopArray = event.target.dataset.streetKey;
-    fetch(
-      `https://api.winnipegtransit.com/v3/stops.json?street=${stopArray}&api-key=${apiKey}`
-    )
+    streetKey = event.target.dataset.streetKey;
+    fetch(`https://api.winnipegtransit.com/v3/stops.json?street=${streetKey}&api-key=${apiKey}`)
       .then((resp) => resp.json())
       .then((data) => {
-        let stopsObject = data.stops;
-        for(const key in stopsObject) {
+        stopsObject = data.stops;
+        console.log(stopsObject[0].key)
+      fetch(`https://api.winnipegtransit.com/v3/stops/${stopsObject[0].key}/schedule.json?max-results-per-route=1&api-key=${apiKey}`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        stopScheduleObject = data;
+        console.log(stopScheduleObject['stop-schedule']['route-schedules'][0]['scheduled-stops'][0].times['departure']['scheduled']);
+        titleBar.innerHTML = "";
+        titleBar.insertAdjacentHTML("afterbegin", `
+          <div id="street-name" class="titlebar">
+          Displaying results for ${stopsObject[0].street.name}
+          </div>
+        `)
+        for (const key in stopsObject) {
           tbody.innerHTML = "";
           tbody.insertAdjacentHTML('afterbegin', `
           <td>${stopsObject[key].street.name}</td>
             <td>${stopsObject[key]["cross-street"].name}</td>
             <td>${stopsObject[key].direction}</td>
             <td>74</td>
-            <td>02:25 PM</td>`)
-          console.log(stopsObject[key])
+            <td>${stopScheduleObject['stop-schedule']['route-schedules'][0]['scheduled-stops'][0].times['departure']['scheduled']}</td>`)
         }
-      });
+      })
+    })
   }
 };
 
